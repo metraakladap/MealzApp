31467c981757b758d1155b447015fa435ce8dba8
metraakaldap <metraakladap228@gmail.com>
2023-05-25 08:43:14 +0300

    worked with list of meals and displayed them

--- app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -3,9 +3,12 @@ package com.example.mealzapp.ui.meals
 import androidx.lifecycle.ViewModel
 import com.example.model.MealsRepository
 import com.example.model.response.MealResponse
+import com.example.model.response.MealsCategoriesResponses
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()) : ViewModel() {
-    fun getMeals(): List<MealResponse>{
-        return repository.getMeals().categories
+    fun getMeals(successCallback: (response: MealsCategoriesResponses?)-> Unit){
+        repository.getMeals() { response ->
+            successCallback(response)
+        }
     }
 }
--- app/src/main/java/com/example/model/MealsRepository.kt
+++ app/src/main/java/com/example/model/MealsRepository.kt
@@ -2,9 +2,26 @@ package com.example.model
 
 import com.example.model.api.MealsWebService
 import com.example.model.response.MealsCategoriesResponses
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MealsRepository(private val webService: MealsWebService = MealsWebService()) {
-    fun getMeals(): MealsCategoriesResponses? {
-        return webService.getMeals().execute().body() // Bad practice
+    fun getMeals(successCallback: (response: MealsCategoriesResponses?)-> Unit) {
+        return webService.getMeals().enqueue(object: Callback<MealsCategoriesResponses>{
+            override fun onResponse(
+                call: Call<MealsCategoriesResponses>,
+                response: Response<MealsCategoriesResponses>
+            ) {
+                if(response.isSuccessful)
+                    successCallback(response.body())
+            }
+
+            override fun onFailure(call: Call<MealsCategoriesResponses>, t: Throwable) {
+
+            }
+        })
+
+
     }
 }
